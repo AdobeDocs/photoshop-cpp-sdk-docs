b6f12046c6dd2813f4451ac56fabd0254ce3a291
Melissa Garcia <melissag@ensemble.com>
2024-11-05 11:17:19 -0800

    rename path prefix

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,7 +11,7 @@
  */
 
 module.exports = {
-  pathPrefix: process.env.PATH_PREFIX || '/photoshop-cpp-sdk/',
+  pathPrefix: process.env.PATH_PREFIX || '/photoshop-cpp-sdk-docs/',
   siteMetadata: {
     versions: [
       {
